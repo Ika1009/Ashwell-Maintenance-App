c4784723f5a37df40ba30fc1235ae3ff2d68286c
Ika1009 <ilija.doncic@yahoo.com>
2023-10-23 22:40:28 +0200

    Apis and creating folder

---
+++ ApiService.cs
@@ -0,0 +1,73 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+public static class ApiService
+{
+    private static readonly string BaseApiUrl = "https://ashwellmaintenance.host";
+
+    /// <summary>
+    /// Uploads a new folder to the server.
+    /// </summary>
+    /// <param name="folderName">The name of the folder to be created.</param>
+    /// <returns>A HttpResponseMessage indicating the outcome of the API call.</returns>
+    public static async Task<HttpResponseMessage> UploadFolderAsync(string folderName)
+    {
+        using HttpClient client = new HttpClient();
+        var folderData = new { folder_name = folderName };
+        var jsonContent = new StringContent(JsonSerializer.Serialize(folderData), Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PostAsync($"{BaseApiUrl}/create_folder.php", jsonContent);
+
+        return response;
+    }
+    /// <summary>
+    /// Uploads a new report to the server.
+    /// </summary>
+    /// <param name="reportType">The type of the report.</param>
+    /// <param name="reportName">The name of the report.</param>
+    /// <param name="folderId">The ID of the folder where the report should be saved.</param>
+    /// <param name="additionalReportData">A dictionary containing real values for the report.</param>
+    /// <returns>A HttpResponseMessage indicating the outcome of the API call.</returns>
+    public static async Task<HttpResponseMessage> UploadReportAsync(string reportType, string reportName, string folderId, Dictionary<string, string> additionalReportData)
+    {
+        using HttpClient client = new();
+        // Combine provided data with additional report data
+        var postData = new Dictionary<string, string>
+        {
+            {"report_type", reportType},
+            {"report_name", reportName},
+            {"folder_id", folderId}
+        }.Concat(additionalReportData);
+
+        var jsonContent = new StringContent(JsonSerializer.Serialize(postData), Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PostAsync($"{BaseApiUrl}/upload_report.php", jsonContent);
+
+        return response;
+    }
+
+    /// <summary>
+    /// Uploads two images signatures to the server along with folder ID and folder name.
+    /// </summary>
+    /// <param name="image1">The first image to be uploaded.</param>
+    /// <param name="image2">The second image to be uploaded.</param>
+    /// <param name="folderId">The ID of the folder where the images should be saved.</param>
+    /// <param name="folderName">The name of the folder where the images should be saved.</param>
+    /// <returns>A HttpResponseMessage indicating the outcome of the API call.</returns>
+    public static async Task<HttpResponseMessage> UploadImagesAsync(Stream image1, Stream image2, string folderId, string folderName)
+    {
+        using HttpClient client = new();
+        using MultipartFormDataContent content = new()
+        {
+            // Adding the two images with generated names
+            { new StreamContent(image1), "signature1", $"{folderName}_{folderId}.jpg" },
+            { new StreamContent(image2), "signature2", $"{folderName}_{folderId}.jpg" },
+        };
+
+        HttpResponseMessage response = await client.PostAsync($"{BaseApiUrl}/upload_signatures.php", content);
+
+        return response;
+    }
+}
--- View/NewFolderPopup.xaml.cs
+++ View/NewFolderPopup.xaml.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Maui.Views;
+using System.Text.Json;
 
 namespace Ashwell_Maintenance.View;
 
@@ -13,4 +14,49 @@ public partial class NewFolderPopup : Popup
 	{
 		this.Close();
 	}
+    public async void NewFolderClicked(object sender, EventArgs e)
+    {
+        // Check if folder name is empty or consists only of whitespace
+        if (string.IsNullOrWhiteSpace(folderName.Text))
+        {
+            await Shell.Current.DisplayAlert("Error", "Folder name cannot be empty.", "OK");
+            return;
+        }
+
+        try
+        {
+            var response = await ApiService.UploadFolderAsync(folderName.Text);
+
+            if (response.IsSuccessStatusCode)
+                this.Close();
+            else
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+
+                string errorMessage;
+                if (!string.IsNullOrWhiteSpace(errorContent))
+                {
+                    try
+                    {
+                        var errorObj = JsonSerializer.Deserialize<Dictionary<string, string>>(errorContent);
+                        errorMessage = errorObj.ContainsKey("error") ? errorObj["error"] : "An unknown error occurred.";
+                    }
+                    catch
+                    {
+                        errorMessage = "An unknown error occurred.";
+                    }
+                }
+                else
+                    errorMessage = "Internal server error.";
+
+                await Shell.Current.DisplayAlert("Error", errorMessage, "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle other potential exceptions like network errors, timeouts, etc.
+            await Shell.Current.DisplayAlert("Error", $"An error occurred: {ex.Message}", "OK");
+        }
+
+    }
 }
