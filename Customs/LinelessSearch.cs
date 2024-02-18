69ce739918ce18c9e0fdb687f8b60418f20b4517
joemama <nikola.andjelkovic.nix@gmail.com>
2024-02-18 21:30:00 +0100

    izbacen numeric za datume i dodat search bar na displayed porojects strani

---
+++ Customs/LinelessSearch.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ashwell_Maintenance.Customs
+{
+    class LinelessSearch : SearchBar
+    {
+        static LinelessSearch()
+        {
+
+        }
+    }
+}
